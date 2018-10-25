b52d6234c677f176b2f8a4d64538914556d5dbbe
Subrata <saha.subrata@gmail.com>
2018-10-25 22:32:10 +0530

    added private and public subnet using internet and nat gateway

--- compute.tf
+++ compute.tf
@@ -1,16 +1,13 @@
 data "aws_ami" "ubuntu" {
   most_recent = true
-
   filter {
     name   = "name"
     values = ["ubuntu/images/hvm-ssd/ubuntu-trusty-14.04-amd64-server-*"]
   }
-
   filter {
     name   = "virtualization-type"
     values = ["hvm"]
   }
-
   owners = ["099720109477"]
 }
 
@@ -23,4 +20,15 @@ resource "aws_instance" "kms_compute_machine" {
   }
 
   subnet_id = "${aws_subnet.kms_public_subnet.id}"
+}
+
+resource "aws_instance" "kms_db_compute_machine" {
+  ami = "${data.aws_ami.ubuntu.id}"
+  instance_type = "t2.micro"
+
+  tags {
+    Name = "kmsprivateresource"
+  }
+
+  subnet_id = "${aws_subnet.kms_private_subnet.id}"
 }
--- network.tf
+++ network.tf
@@ -14,4 +14,44 @@ resource "aws_subnet" "kms_public_subnet" {
   cidr_block        = "${var.cidrblocksn2}"
   vpc_id            = "${aws_vpc.kms_main_vpc.id}"
   availability_zone = "${var.az["az2"]}"
+  map_public_ip_on_launch = false
+}
+
+resource "aws_internet_gateway" "kms_internet_gateway" {
+  vpc_id = "${aws_vpc.kms_main_vpc.id}"
+}
+
+resource "aws_route_table" "kms_public_route_table" {
+  vpc_id = "${aws_vpc.kms_main_vpc.id}"
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = "${aws_internet_gateway.kms_internet_gateway.id}"
+  }
+}
+
+resource "aws_route_table_association" "kms_rt_association" {
+  route_table_id = "${aws_route_table.kms_public_route_table.id}"
+  subnet_id = "${aws_subnet.kms_public_subnet.id}"
+}
+
+resource "aws_eip" "kms_nat_gateway_elastic_ip" {
+  vpc = true
+}
+
+resource "aws_nat_gateway" "kms_nat_gateway" {
+  allocation_id = "${aws_eip.kms_nat_gateway_elastic_ip.id}"
+  subnet_id = "${aws_subnet.kms_public_subnet.id}"
+}
+
+resource "aws_route_table" "kms_private_route_table" {
+  vpc_id = "${aws_vpc.kms_main_vpc.id}"
+  route {
+    cidr_block = "0.0.0.0/0"
+    nat_gateway_id = "${aws_nat_gateway.kms_nat_gateway.id}"
+  }
+}
+
+resource "aws_route_table_association" "kms_private_subnet_route_association" {
+  route_table_id = "${aws_route_table.kms_private_route_table.id}"
+  subnet_id = "${aws_subnet.kms_private_subnet.id}"
 }
