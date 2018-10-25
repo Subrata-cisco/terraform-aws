7929d32eb1e088bab478604661a5e0b6ac7b3710
Subrata <saha.subrata@gmail.com>
2018-10-25 22:33:02 +0530

    formatted

--- compute.tf
+++ compute.tf
@@ -1,13 +1,16 @@
 data "aws_ami" "ubuntu" {
   most_recent = true
+
   filter {
     name   = "name"
     values = ["ubuntu/images/hvm-ssd/ubuntu-trusty-14.04-amd64-server-*"]
   }
+
   filter {
     name   = "virtualization-type"
     values = ["hvm"]
   }
+
   owners = ["099720109477"]
 }
 
@@ -23,7 +26,7 @@ resource "aws_instance" "kms_compute_machine" {
 }
 
 resource "aws_instance" "kms_db_compute_machine" {
-  ami = "${data.aws_ami.ubuntu.id}"
+  ami           = "${data.aws_ami.ubuntu.id}"
   instance_type = "t2.micro"
 
   tags {
--- network.tf
+++ network.tf
@@ -11,9 +11,9 @@ resource "aws_subnet" "kms_private_subnet" {
 }
 
 resource "aws_subnet" "kms_public_subnet" {
-  cidr_block        = "${var.cidrblocksn2}"
-  vpc_id            = "${aws_vpc.kms_main_vpc.id}"
-  availability_zone = "${var.az["az2"]}"
+  cidr_block              = "${var.cidrblocksn2}"
+  vpc_id                  = "${aws_vpc.kms_main_vpc.id}"
+  availability_zone       = "${var.az["az2"]}"
   map_public_ip_on_launch = false
 }
 
@@ -23,6 +23,7 @@ resource "aws_internet_gateway" "kms_internet_gateway" {
 
 resource "aws_route_table" "kms_public_route_table" {
   vpc_id = "${aws_vpc.kms_main_vpc.id}"
+
   route {
     cidr_block = "0.0.0.0/0"
     gateway_id = "${aws_internet_gateway.kms_internet_gateway.id}"
@@ -31,7 +32,7 @@ resource "aws_route_table" "kms_public_route_table" {
 
 resource "aws_route_table_association" "kms_rt_association" {
   route_table_id = "${aws_route_table.kms_public_route_table.id}"
-  subnet_id = "${aws_subnet.kms_public_subnet.id}"
+  subnet_id      = "${aws_subnet.kms_public_subnet.id}"
 }
 
 resource "aws_eip" "kms_nat_gateway_elastic_ip" {
@@ -40,18 +41,19 @@ resource "aws_eip" "kms_nat_gateway_elastic_ip" {
 
 resource "aws_nat_gateway" "kms_nat_gateway" {
   allocation_id = "${aws_eip.kms_nat_gateway_elastic_ip.id}"
-  subnet_id = "${aws_subnet.kms_public_subnet.id}"
+  subnet_id     = "${aws_subnet.kms_public_subnet.id}"
 }
 
 resource "aws_route_table" "kms_private_route_table" {
   vpc_id = "${aws_vpc.kms_main_vpc.id}"
+
   route {
-    cidr_block = "0.0.0.0/0"
+    cidr_block     = "0.0.0.0/0"
     nat_gateway_id = "${aws_nat_gateway.kms_nat_gateway.id}"
   }
 }
 
 resource "aws_route_table_association" "kms_private_subnet_route_association" {
   route_table_id = "${aws_route_table.kms_private_route_table.id}"
-  subnet_id = "${aws_subnet.kms_private_subnet.id}"
+  subnet_id      = "${aws_subnet.kms_private_subnet.id}"
 }
